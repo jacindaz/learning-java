21b0483ea7f1bd90c744ea7f0ed5161764dc9233
Jacinda Zhong <jacindaz@gmail.com>
2014-08-23 12:56:59 -0400

    Using switch and do-while loops

--- Help.java
+++ Help.java
@@ -1,14 +1,19 @@
 class Help {
     public static void main(String args[])
         throws java.io.IOException {
-        char choice;
-        
-        System.out.println("Help on:");
-        System.out.println(" 1. if");
-        System.out.println(" 2. switch");
-        System.out.print("Choose one: ");
-        choice = (char) System.in.read();
-        
+        char choice, ignore;
+
+        do {
+            System.out.println("Help on:");
+            System.out.println(" 1. if");
+            System.out.println(" 2. switch");
+            System.out.println(" 3. for");
+            System.out.println(" 4. while");
+            System.out.println(" 5. do-while\n");
+            System.out.print("Choose one: ");
+            choice = (char) System.in.read();
+        } while( choice < '1' | choice > '5');
+
         switch(choice) {
             case '1':
                 System.out.println("The if:\n");
@@ -24,8 +29,24 @@ class Help {
                 System.out.println("    // ...");
                 System.out.println("}");
                 break;
+            case '3':
+                System.out.println("The for:\n");
+                System.out.println("for(init; condition; iteration)");
+                System.out.println(" statement;");
+                break;
+            case '4':
+                System.out.println("The while:\n");
+                System.out.println("while(condition) statement;");
+                break;
+            case '5':
+                System.out.println("The do-while:\n");
+                System.out.println("do {");
+                System.out.println(" statement;");
+                System.out.println("} while(condition);");
+                break;
             default:
                 System.out.println("Selection not found.");
         }
+
     }
 }

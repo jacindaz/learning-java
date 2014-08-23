ad904b1ca6eafb5381cc2e0f0a6b12676dcfa055
Jacinda Zhong <jacindaz@gmail.com>
2014-08-23 14:03:58 -0400

    Creates HelpMethods class, displays menu twice (bug)

--- Help.java
+++ Help.java
@@ -1,20 +1,22 @@
-class Help {
-    public static void main(String args[])
-        throws java.io.IOException {
-        char choice, ignore;
+class HelpMethods {
 
-        do {
-            System.out.println("Help on:");
-            System.out.println(" 1. if");
-            System.out.println(" 2. switch");
-            System.out.println(" 3. for");
-            System.out.println(" 4. while");
-            System.out.println(" 5. do-while\n");
-            System.out.print("Choose one: ");
-            choice = (char) System.in.read();
-        } while( choice < '1' | choice > '5');
+    void showmenu() {
+        System.out.println("showmenu method");
 
-        switch(choice) {
+        System.out.println("Help on:");
+        System.out.println(" 1. if");
+        System.out.println(" 2. switch");
+        System.out.println(" 3. for");
+        System.out.println(" 4. while");
+        System.out.println(" 5. do-while");
+        System.out.println(" 6. break");
+        System.out.println(" 7. continue\n");
+        System.out.print("Choose one (q to quit): ");
+    }
+
+    void helpon(char what) {
+        System.out.println("helpon method");
+        switch(what) {
             case '1':
                 System.out.println("The if:\n");
                 System.out.println("if(condition) statement;");
@@ -44,9 +46,48 @@ class Help {
                 System.out.println(" statement;");
                 System.out.println("} while(condition);");
                 break;
-            default:
-                System.out.println("Selection not found.");
+            case '6':
+                System.out.println("The break:\n");
+                System.out.println("break; or break label;");
+                break;
+            case '7':
+                System.out.println("The continue:\n");
+                System.out.println("continue; or continue label;");
+                break;
         }
+    }
 
+    boolean isvalid(char ch) {
+        System.out.println("isvalid method");
+        if(ch < '1' | ch > '7' | ch == 'q') return false;
+        else return true;
+    }
+}
+
+class Help {
+    public static void main(String args[])
+        throws java.io.IOException, InterruptedException {
+        char choice, ignore;
+        HelpMethods help_object = new HelpMethods();
+
+        System.out.println("Inside main method");
+
+        do {
+            System.out.println("Inside do loop");
+
+            help_object.showmenu();
+            choice = (char) System.in.read();
+
+            System.out.println("User choice: " + choice);
+
+            if (help_object.isvalid(choice)) {
+                help_object.helpon(choice);
+                Thread.sleep(1000);
+                System.out.println("=================================================\n");
+            }
+
+        } while(choice != 'q');
+
+        System.out.println("Exiting from help menu...");
     }
 }
